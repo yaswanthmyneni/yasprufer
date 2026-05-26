3123e21bab4d355f997f9ff00cb3faeefba43652
Yaswanth Myneni <79623546+yaswanthmyneni@users.noreply.github.com>
2026-05-26 07:55:38 +0530

    removed try catch in db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
 
 const connectToDB = async () => {
-  try {
-    if (!process.env.MONGODB_URI) {
-      throw new Error("MongoDB URI is required");
-    }
-
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to DB successfully!");
-  } catch (error) {
-    console.log(error);
+  if (!process.env.MONGODB_URI) {
+    throw new Error("MongoDB URI is required");
   }
+
+  await mongoose.connect(process.env.MONGODB_URI);
+
+  console.log("Connected to DB successfully!");
 };
 
 export { connectToDB };
