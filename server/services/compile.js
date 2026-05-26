d34ac6af954cc76224b4f92e9ff20a09ae892aac
Yaswanth Myneni <79623546+yaswanthmyneni@users.noreply.github.com>
2026-05-26 15:35:19 +0530

    Update compile.js

--- server/services/compile.js
+++ server/services/compile.js
@@ -5,6 +5,7 @@ const COMPILE_BASE_URL =
   process.env.COMPILE_URL || `${process.env.COMPILER_URL}`;
 
 const runCode = (data) => {
+  console.log(`${COMPILE_BASE_URL}/run`);
   return axios.post(`${COMPILE_BASE_URL}/run`, data);
 };
 
